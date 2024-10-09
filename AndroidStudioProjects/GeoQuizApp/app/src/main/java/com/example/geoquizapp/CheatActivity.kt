bc1fad2c8310e33f87154cd770957e1a3de6b3d5
glotshik <moitshiikx@gmail.com>
2024-10-08 20:39:30 -0400

    chap 7 complete

---
+++ AndroidStudioProjects/GeoQuizApp/app/src/main/java/com/example/geoquizapp/CheatActivity.kt
@@ -0,0 +1,50 @@
+package com.example.geoquizapp
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.geoquizapp.databinding.ActivityCheatBinding
+
+class CheatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheatBinding
+    private var answerIsTrue = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        binding.showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            binding.answerTextView.setText(answerText)
+            setAnswerShowResult(true)
+        }
+    }
+
+    private fun setAnswerShowResult(isAnswerShow: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShow)
+        }
+        setResult(Activity.RESULT_OK, data)
+    }
+
+    companion object {
+        private const val EXTRA_ANSWER_IS_TRUE = "com.example.geoquizzapp.answer_is_true"
+        const val EXTRA_ANSWER_SHOWN = "com.example.geoquizzapp.answer_shown"  // Note: Changed this string
+
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+}
